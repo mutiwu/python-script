0b9d64b8f756b44328872a881d8b0b4ba76430b4
Qian Guo <qiguo@redhat.com>
2015-07-08 04:44:53 +0800

    support rhel6

--- easy_get.py
+++ easy_get.py
@@ -15,59 +15,92 @@ class Package(object):
         self.p_name = p_name
         self.build = build
         self.direc = direc
-        self.supportComp = [
+        self.sptcmp = [
                 "qemu-kvm-rhev",
                 "qemu-kvm",
                 "kernel",
                 ]
-        self.supportBuild = [
+        self.sptbld = [
                 "rhel6",
                 "rhel7",
                 ]
         self.__p_dict = {
-                self.supportComp[0]: "35491",
-                self.supportComp[1]: "18283",
-                self.supportComp[2]: "1231",
+                self.sptcmp[0]: "35491",
+                self.sptcmp[1]: "18283",
+                self.sptcmp[2]: "1231",
                 }
         self.__b_dict = {
-                self.supportBuild[1]: r"buildID=(\d+).*?el7",
-                self.supportBuild[0]: r"buildID=(\d+).*?el6",
+                self.sptbld[1]: r"buildID=(\d+).*?el7",
+                self.sptbld[0]: r"buildID=(\d+).*?el6",
                 }
-        self.__kernel_list = [
-                "kernel",
-                "kernel-debuginfo",
-                "kernel-debuginfo-common",
-                ]
-        self.__qemu_rhev_list = [
-                "qemu-img-rhev",
-                "libcacard-devel-rhev",
-                "libcacard-rhev",
-                "libcacard-tools-rhev",
-                "qemu-kvm-common-rhev",
-                "qemu-kvm-rhev",
-                "qemu-kvm-tools-rhev",
-                "qemu-kvm-rhev-debuginfo",
-                ]
-        self.__qemu_list = [
-                ""
-                ]
+        self.__kernel_list = {
+                self.sptbld[1]: [
+                    "kernel",
+                    "kernel-debuginfo",
+                    "kernel-debuginfo-common",
+                ],
+                self.sptbld[0]: [
+                    "kernel",
+                    "kernel-debuginfo",
+                    "kernel-debuginfo-common",
+                ],
+                }
+        self.__qemu_rhev_list = {
+                self.sptbld[1]: [
+                    "qemu-kvm-rhev",
+                    "qemu-img-rhev",
+                    "libcacard-devel-rhev",
+                    "libcacard-rhev",
+                    "libcacard-tools-rhev",
+                    "qemu-kvm-common-rhev",
+                    "qemu-kvm-tools-rhev",
+                    "qemu-kvm-rhev-debuginfo",
+                ],
+                self.sptbld[0]: [
+                    "qemu-kvm-rhev",
+                    "qemu-img-rhev",
+                    "qemu-kvm-rhev-tools",
+                    "qemu-kvm-rhev-debuginfo",
+                ],
+                }
+        self.__qemu_list = {
+                self.sptbld[1]: [
+                    "qemu-kvm",
+                    "libcacard",
+                    "libcacard-devel",
+                    "libcacard-tools",
+                    "qemu-img",
+                    "qemu-kvm-common",
+                    "qemu-kvm-tools",
+                    "qemu-kvm-debuginfo",
+                ],
+                self.sptbld[0]: [
+                    "qemu-kvm",
+                    "qemu-kvm-tools",
+                    "qemu-kvm-debuginfo",
+                    "qemu-img",
+                    "qemu-guest-agent",
+                ],
+                }
         self.__rpm_dict = {
-                self.supportComp[2]: self.__kernel_list,
-                self.supportComp[0]: self.__qemu_rhev_list,
-                self.supportComp[1]: self.__qemu_list,
+                self.sptcmp[2]: self.__kernel_list,
+                self.sptcmp[0]: self.__qemu_rhev_list,
+                self.sptcmp[1]: self.__qemu_list,
                 }
         self.__pattern_rpm = r"href=\"(http.*?%s.*?\.x86_64\.rpm)"
+        self.__pattern_fw = r"href=\"(http.*?%s\-firmware.*?\.noarch\.rpm)"
         self.__baseurl = "https://brewweb.devel.redhat.com/"
         self.__packageinfo = "packageinfo?packageID="
         self.__buildinfo = "buildinfo?buildID="
-        self.__errormsg = "Only support %s" % ','.join(self.supportComp)
+        self.__errormsg1 = "Only support %s" % ','.join(self.sptcmp)
+        self.__errormsg2 = "Only support %s " % ','.join(self.sptbld)
 
-    def __rpm_list(self):
+    def __rpm_list_dict(self):
         '''
         provide the corresponding component's rpm list.
         '''
         if self.p_name not in self.__rpm_dict.keys():
-            print self.__errormsg
+            print self.__errormsg1
             sys.exit(1)
         return self.__rpm_dict[self.p_name]
 
@@ -76,14 +109,14 @@ class Package(object):
         provide the html, and the buildid in the brew
         '''
         if self.p_name not in self.__p_dict.keys():
-            print self.__errormsg
+            print self.__errormsg1
             sys.exit(1)
         self.p_id = self.__p_dict[self.p_name]
         self.packageurl = self.__baseurl + self.__packageinfo + self.p_id
         self.p_open = urllib2.urlopen(self.packageurl)
         self.p_html = self.p_open.read()
         if self.build not in self.__b_dict.keys():
-            print "Only support rhel6, rhel7"
+            print self.__errormsg2
             sys.exit(1)
         self.p_build = self.__b_dict[self.build]
         self.buildid = re.search(self.p_build, self.p_html).groups()[0]
@@ -95,11 +128,14 @@ class Package(object):
         '''
         download the rpms.
         '''
-        self.r_list = self.__rpm_list()
-        self.direcname = self.r_list[0]
+        self.r_list_dict = self.__rpm_list_dict()
+        if self.build not in self.r_list_dict.keys():
+            print self.__errormsg2
+        self.r_list = self.r_list_dict[self.build]
+        self.dirname = self.r_list[0]
         self.b_html, self.buildid = self.__buildhtml()
         self.path = self.direc
-        self.path += "%s%s" % (self.direcname, self.buildid)
+        self.path += "%s%s" % (self.dirname, self.buildid)
         if os.path.exists(self.path):
             print "The directory exists, please choose another directory."
             sys.exit(3)
@@ -114,6 +150,15 @@ class Package(object):
                 print "wget hit error, need check"
                 sys.exit(2)
             print "%s is downloaded" % self.rpm
+        if (self.build == self.sptbld[0]) and (self.dirname == self.sptcmp[2]):
+            self.p_fw = re.compile(self.__pattern_fw % self.sptcmp[2])
+            self.fw_url = self.p_fw.findall(self.b_html)[0]
+            print "will download %s" % self.fw_url
+            self.status = commands.getstatusoutput("wget %s" % self.fw_url)[0]
+            if self.status:
+                print "wget hit error, need check"
+                sys.exit(2)
+            print "%s\-firmware is downloaded" % self.sptcmp[2]
         return self.path
 
     def install_rpms(self):
@@ -123,12 +168,12 @@ class Package(object):
         self.path = self.wget_rpms()
         os.chdir(self.path)
         self.cmd = {
-                self.supportComp[0]: "yum install * -y",
-                self.supportComp[1]: "yum install * -y",
-                self.supportComp[2]: "rpm -ivh *",
+                self.sptcmp[0]: "yum install * -y",
+                self.sptcmp[1]: "yum install * -y",
+                self.sptcmp[2]: "rpm -ivh *",
                 }
         if self.p_name not in self.cmd.keys():
-            print self.__errormsg
+            print self.__errormsg1
         self.status = commands.getstatusoutput(self.cmd[self.p_name])[0]
         if self.status:
             print "Install hit error, need check"
