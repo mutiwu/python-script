9ea241215647c6b2fb499ea96491fb5b989b04f5
Qian Guo <qiguo@redhat.com>
2015-07-08 18:59:25 +0800

    modify some

--- top_check_vhost.py
+++ top_check_vhost.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
 import os
+import sys
 
-pid_vhost = os.popen('pgrep vhost')
+pid_vhost = os.popen('pidof vhost')
 
 list_pids = pid_vhost.readlines()
 if list_pids == [] :
     print '\nQuit:No vhost threads available.\n'
-    os._exit(0)
+    sys.exit(0)
 print 'list_pids:', list_pids
 list_pids_str = ','.join([i.strip('\n') for i in list_pids])
 os.system('top -p %s' % list_pids_str)
-
 print 'top result', list_pids_str
    
 
