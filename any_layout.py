a0d6a1d1a0e475016f2940a01f029e9a014637c5
root <root@dhcp-9-183.nay.redhat.com>
2015-07-21 17:57:09 +0800

    fix wrong bit

--- any_layout.py
+++ any_layout.py
@@ -3,6 +3,7 @@ import sys
 import commands
 
 iface = sys.argv[1]
+any_layout_bit = "27"
 cmd = "cat /sys/class/net/%s/device/features" % iface
 status, output = commands.getstatusoutput(cmd)
 print "# TOTAL %s bits." % len(output)
@@ -11,6 +12,6 @@ for i in range(len(output)):
     result.append(("Bit %s" % i, "value %s" % output[i]))
 print "\n# They are:\n%s" % result
 for name, value in result:
-    if name == "Bit %s" % "26":
-        print "\n# The 26th bit's  %s" % value
+    if name == "Bit %s" % any_layout_bit:
+        print "\n# The 27th bit's  %s" % value
 
