bb826f45ebe2ba946ccb7f434ad57c652a4e50ae
guoqian <guoqian@nco-china.com>
2016-04-27 13:52:44 +0800

    add per_netperf

---
+++ per_netperf.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# coding=utf-8
+import commands
+import os
+import re
+import time
+
+dirdata = time.ctime().replace(' ', '_')
+tmpath = "/tmp/per_net%s" %dirdata
+
+if os.path.exists(tmpath):
+    print "won't create new dir, quit the script."
+    os.sys.exit(1)
+os.mkdir(tmpath)
+optl = 5
+insts = 5
+proto = ["TCP_STREAM", "TCP_MAERTS"]
+psize = ["64", "4096", "65535"]
+server_ip = "172.30.132.70"
+cmd = "taskset -c %s netperf -H %s -l %s -C -c -t %s -- -m %s >>%s & "
+for t in proto:
+    finallist = ['Size', t, '\n']
+    tpath = "%s/%s" %(tmpath, t)
+    if os.path.exists(tpath):
+        print "%s already exists, quit the script." %tpath
+        os.sys.exit(1)
+    os.mkdir(tpath)
+    finaldata = "%s/finaldata_%s" %(tpath, t)
+    f = file(finaldata, "a+")
+    f.writelines(finallist)
+    f.close()
+    for ps in psize:
+        stpath = "%s/%s" %(tpath, ps)
+        if os.path.exists(stpath):
+            print "%s already exists, quit the script." %stpath
+            os.sys.exit(1)
+        os.mkdir(stpath)
+        stlist = []
+        for c in range(1, 6):
+            countfile = "%s//rs_%s_%s_c%s" %(stpath, t, ps, c)
+            for i in range(1, (int(insts) + 1)):
+#                status, output = commands.getstatusoutput(cmd %(i, server_ip, optl, t, ps, countfile))
+                if os.system(cmd %(i, server_ip, optl, t, ps, countfile)):
+                    print "Netperf hit wrong, quit"
+                    os.sys.exit
+            sleep_time = int(optl) + 3
+            time.sleep(sleep_time)
+            f = file(countfile, "r")
+            tmpstr = 'flag'.join(f.readlines())
+            f.close()
+            pp = re.compile(r"\nflag\s+\d+\s+\d+\s+\d+\s+\d+\.\d+\s+(\d+\.\d+)")
+            tmplist = pp.findall(tmpstr)
+            stlist.append(float(sum([float(i) for i in tmplist])))
+        stfinalrs = sum(stlist)/5
+        valuelist = [ps, '\t', str(stfinalrs), '\n']
+        f = file(finaldata, "a+")
+        f.writelines(valuelist)
+        f.close()
+
