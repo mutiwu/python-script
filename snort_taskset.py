da9f9581752dc7368b01e38933828cde4e5492a5
guoqian <guoqian@nco-china.com>
2016-06-03 15:39:56 +0800

    add top to snort taskset

---
+++ snort_taskset.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import commands
+import sys
+import re
+import os
+import argparse
+
+parser= argparse.ArgumentParser()
+parser.add_argument("int", metavar="N", type=int, help="The first CPU")
+args = parser.parse_args(sys.argv[1:])
+if len(sys.argv[1:]) > 2:
+    print "More than 1 args found, only 1 arg supported."
+    parser.print_help()
+    sys.exit(0)
+
+int_cpu = args.int
+cmd = "pgrep snort"
+status, output = commands.getstatusoutput(cmd)
+if status:
+    print "pgrep hit error,exit."
+    print output
+    sys.exit(1)
+if output == "":
+    print "no snort exist, exit"
+    sys.exit(0)
+pid_snort = int(output)
+cmd = "pstree -p %s" %pid_snort
+status, output = commands.getstatusoutput(cmd)
+if status:
+    print "pstree hit error, exit"
+    sys.exit(1)
+re_p = re.compile(r".*?\((\d+)\)")
+plist = re_p.findall(output)
+psize = len(plist)
+for i in range(psize):
+    cpu = int_cpu + i
+    cmd = "taskset -cp %s %s" % (cpu, plist[i])
+    status, output = commands.getstatusoutput(cmd)
+    if status:
+        print "taskset hit error"
+        print output
+        sys.exit(1)
+    print output
+p_str = ",".join(plist)
+cmd = "top -p %s" % p_str
+os.system(cmd)
