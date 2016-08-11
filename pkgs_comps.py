4f9946e8717dd71c15bbb47e0150d15a831e5fe0
guoqian <guoqian@nco-china.com>
2016-08-11 19:15:04 +0800

    update comps

--- pkgs_comps.py
+++ pkgs_comps.py
@@ -4,6 +4,7 @@ import commands
 import xml.etree.ElementTree as ET
 import argparse
 import sys
+import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-s",
@@ -23,53 +24,87 @@ parser.add_argument("-d",
                     dest="destiso",
                     metavar="FILEPATH"
                    )
+parser.add_argument("-c",
+		    action="store",
+		    dest="cmppath",
+		    metavar="FILEPATH"
+		   )
 args = parser.parse_args(sys.argv[1:])
 
 
 xmlpath = args.xmlpath
 srciso = args.srciso
 dstreiso = args.destiso
+cmppath = args.cmppath
 Ppath = "/Packages"
 Rpath = "/repodata"
 srcP = args.srciso + Ppath
+cmpP = args.cmppath + Ppath
 dstP = dstreiso + Ppath
 dstR = dstreiso + Rpath
 dstcompsxml = dstR + "/comps.xml"
-cmd = "mkdir -p %s" %dstreiso
-commands.getstatusoutput(cmd)
 
-cmd = "cp -a %s/* %s/" %(srciso, dstreiso)
-print cmd
-commands.getstatusoutput(cmd)
+opl = os.listdir(srcP)
+oplstr = "\n".join(opl)
+print oplstr
+def cmd_handle(cmd):
+    status, output = commands.getstatusoutput(cmd)
+    if status:
+	print output
+	__import__("shutil").rmtree(dstreiso)
+	os.sys.exit(1)
+    print cmd
+cmd = "mkdir -p %s" %dstreiso
+cmd_handle(cmd)
+cmd = "cp -a %s/* %s/" %(cmppath, dstreiso)
+cmd_handle(cmd)
 cmd = "rm -rf %s/*" %dstR
-print cmd
-commands.getstatusoutput(cmd)
+cmd_handle(cmd)
 cmd = "rm -rf %s/*" %dstP
-print cmd
-commands.getstatusoutput(cmd)
+cmd_handle(cmd)
 cmd = "cp %s %s" %(xmlpath, dstcompsxml)
-print cmd
-commands.getstatusoutput(cmd)
+cmd_handle(cmd)
 
 xmltree = ET.parse(dstcompsxml)
 compsrootele = xmltree.getroot()
 glst = compsrootele.findall("group")
-gdict = {}
+#gdict = {}
 for g in glst:
-    id = g.find("id")
-    id.text
+    gid = g.find("id")
+    print gid.text
     ple = g.find("packagelist")
-    pks = ple.getiterator("packagereq")
-    pl = []
+    pks = ple.getchildren()
+    to_del_list = []
     for pk in pks:
-        pl.append(pk.text)
+	if pk.text not in oplstr:
+	    print pk.text
+	    to_del_list.append(pk)
+
+    print to_del_list
+    for pk in to_del_list:
+	print pk
+	pks.remove(pk)
+#	__import__("shutil").rmtree(dstreiso)
+#        pl.append(pk.text)
+    ET.dump(ple)
+    for pk in pks:
         pkt = pk.text + "*"
         cmd = "cp %s/%s %s/" %(srcP, pkt, dstP )
-        print cmd
-        commands.getstatusoutput(cmd)
-    gdict[id] = pl
-print gdict
-
+	cmd_handle(cmd)
+#    gdict[id.text] = pl
+# print gdict
+npl = os.listdir(dstP)
+cpl = os.listdir(cmpP)
+diff_pl = []
+for i in cpl:
+    if i not in npl:
+	diff_pl.append(i)
+if "TRANS.TBL" in diff_pl:
+    diff_pl.remove("TRANS.TBL")
+print "\n".join(diff_pl)
+for dp in diff_pl:
+    cmd = "cp %s/%s %s" %(cmpP, dp, dstP)
+    cmd_handle(cmd)
 
 cmd = "createrepo -g %s %s" %(dstcompsxml, dstreiso )
-commands.getstatusoutput(cmd)
+cmd_handle(cmd)
